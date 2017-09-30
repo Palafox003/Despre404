349c05f72913db74fa0c31a49d7e4c0f899bbc28
Palafox003 <israelpalafox@utgz.edu.mx>
2017-09-30 12:40:32 -0500

    Initial commit

---
+++ ejemploDb/db/class.db.php
@@ -0,0 +1,35 @@
+<?php 
+class Db{
+	private $server="localhost";
+	private $user="root";
+	private $pass="";
+	private $db="peliculas";
+	private $con;
+
+	public function __construct(){
+		$this->conectar();
+	}
+
+	private function conectar(){
+		$c=mysqli_connect($this->server,$this->user,$this->pass,$this->db);
+		if(mysqli_connect_errno()){
+			echo "Error de conexión con la Base de dados.";
+			exit;
+		}else{
+			$this->con=$c;
+		}
+	}
+	public function insertar($tabla,$datos){
+		$consulta="insert into $tabla values($datos)";
+			$result=$this->con->query($consulta);
+			if($result){
+				echo '<div class="alert alert-success" role="alert"> Datos Insertados de forma correcta. </div>';
+			}else{
+				echo '<div class="alert alert-danger" role="alert">
+						  Error al Insertar los datos.
+						</div>';
+				}
+	}
+
+}
+?>
